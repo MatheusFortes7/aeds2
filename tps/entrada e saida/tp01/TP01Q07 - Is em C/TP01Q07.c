691b95681041100b3cf3b29073f0467052dc1d6e
Matheus Fortes <79404379+MatheusFortes7@users.noreply.github.com>
2021-08-25 15:43:03 -0300

    arrumando questao 07

--- tps/entrada e saida/tp01/TP01Q07 - Is em C/TP01Q07.c
+++ tps/entrada e saida/tp01/TP01Q07 - Is em C/TP01Q07.c
@@ -2,29 +2,46 @@
 #include <stdlib.h>
 #include <stdbool.h>
 #include <string.h>
+#include <ctype.h>
 
 bool isFim(char s[]){
     return (strlen(s) == 3 && s[0] == 'F' && s[1] == 'I' && s[2] == 'M');
 }
 
 int isVogal(char s[]){
-    for(int i = 0 ; i < strlen(s); i++){ //corre a string armazenando os caracteres de traz para frente
-        if(s[i] != 'a' && s[i] != 'e' && s[i] != 'i' && s[i] != 'o' && s[i] != 'u' ){
-            i = strlen(s);
-            return "NAO";
-        }      
+    char z[] = tolower(s);
+    bool resp = false;
+    for(int i = 0 ; i < strlen(z); i++){ //corre a string armazenando os caracteres de traz para frente
+        if(z[i] != 'a' || z[i] != 'e' || z[i] != 'i' || z[i] != 'o' || z[i] != 'u' ){
+            i = strlen(z);
+            resp = false;
+        } else {
+            resp = true;
+        }     
     }
-    return "SIM";
+    if (resp == false){
+        return "NAO";
+    } else {
+        return "SIM";
+    }
 }
 
 int isConsoante(char s[]){
-    for(int i = 0; i < strlen(s); i++){
-    if(s[i] == 'a' && s[i] == 'e' && s[i] == 'i' && s[i] == 'o' && s[i] == 'u'){
-            i = strlen(s);
-            return "NAO";
+    char z[] = tolower(s);
+    bool resp  = false;
+    for(int i = 0; i < strlen(z); i++){
+    if(z[i] == 'a' || z[i] == 'e' || z[i] == 'i' || z[i] == 'o' || z[i] == 'u'){
+            i = strlen(z);
+            resp = false;
+        } else {
+            resp = true;
         }
     }
-    return "SIM";
+    if (resp == false){
+        return "NAO";
+    } else {
+        return "SIM";
+    }
 }
 
 //FALTA VER SE É INTEIRO (QUE NAO SEI COMO), E QUANDO ELE E U NUMERO REAL <----------------------------------------------------- IMPORTANTE
